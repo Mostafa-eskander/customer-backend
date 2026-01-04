3c3d435ea87c8702f0f623c3745bf6e40a462e81
Mostafa-eskander <mosgadeskander@gmail.com>
2026-01-04 17:15:29 +0200

    Fix2

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const transactionSchema = new mongoose.Schema({
   client: { type: mongoose.Schema.Types.ObjectId, ref: 'Client' },
+  name: String,
   type: { 
     type: String, 
     enum: ['دخل', 'مصروف'],
