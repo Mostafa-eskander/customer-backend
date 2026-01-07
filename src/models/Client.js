ef784e4f17dd205f35e9c84a7f766221b76946ae
Mostafa-eskander <mosgadeskander@gmail.com>
2026-01-07 03:43:36 +0200

    fix Client

--- src/models/Client.js
+++ src/models/Client.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Transaction = require('./Transaction'); // عشان نقدر نحذف المعاملات المرتبطة
 
 const clientSchema = new mongoose.Schema({
   name: String,
@@ -6,5 +7,14 @@ const clientSchema = new mongoose.Schema({
   notes: String,
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
 }, { timestamps: true });
+
+// 👈 قبل حذف العميل، احذف كل المعاملات المرتبطة به
+clientSchema.pre('findOneAndDelete', async function(next) {
+  const doc = await this.model.findOne(this.getFilter());
+  if (doc) {
+    await Transaction.deleteMany({ client: doc._id, user: doc.user });
+  }
+  next();
+});
 
 module.exports = mongoose.model('Client', clientSchema);
